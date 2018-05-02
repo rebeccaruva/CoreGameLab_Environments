dd3bcddb7997124620fa83c439fb52f4e19e3d7e
rebeccaruva <rebeccaruva@newschool.edu>
2018-05-02 11:28:49 -0400

    google cardboard

---
+++ GoogleCardboardPractice/Assets/Scripts/LerpTongue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpTongue : MonoBehaviour {
+    private Transform start;
+    private Transform end;
+    public float speed;
+    private float startTime;
+    private float journeyLength;
+
+	// Use this for initialization
+	void Start () {
+        //randomize
+        start.position = new Vector3(Random.Range(-5f, 5f), Random.Range(-.8f, 53f), Random.Range(-5f, 5f));
+        end.position = new Vector3(Random.Range(-5f, 5f), Random.Range(-.8f, 53f), Random.Range(-5f, 5f));
+
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(start.position, end.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(start.position, end.position, fracJourney);
+    }
+}
---
+++ GoogleCardboardPractice/Assets/Scripts/TongueStretch.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TongueStretch : MonoBehaviour {
+
+    public Vector3 startPosition;
+    public GameObject tongue;
+    public bool mirrorZ;
+    //private RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+        //Stretch(gameObject, startPosition, endPosition, mirrorZ);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+            if(Input.GetMouseButton(0))
+            {
+                tongue.transform.localScale += new Vector3(0f, hit.distance, 0f);
+                //tongue.transform.position += new Vector3(0f, hit.distance / 2, 0f);
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+            tongue.transform.localScale = startPosition;
+        }
+    }
+
+    //public void Stretch(GameObject _sprite, Vector3 _initialPosition, Vector3 _finalPosition, bool _mirrorZ)
+    //{
+    //    Vector3 centerPos = (_initialPosition + _finalPosition) / 2f;
+    //    _sprite.transform.position = centerPos;
+    //    Vector3 direction = _finalPosition - _initialPosition;
+    //    direction = Vector3.Normalize(direction);
+    //    _sprite.transform.right = direction;
+    //    if (_mirrorZ) _sprite.transform.right *= -1f;
+    //    Vector3 scale = new Vector3(1, 1, 1);
+    //    scale.x = Vector3.Distance(_initialPosition, _finalPosition);
+    //    _sprite.transform.localScale = scale;
+    //}
+}
